ee7c71997d57fe89fad8e2aeca007ae8339a2244
Danneborg <whitetheia@gmail.com>
2020-10-06 22:25:34 +0700

    First version

---
+++ src/Component/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg"
+                 alt="Content"/>
+        </div>
+        <div>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRXeeLq87BeFC1UK-L9RDuD_-O0UTic6YNC_A&usqp=CAU"
+                alt="Content"/>
+        </div>
+        <div>My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>Post1</div>
+                <div>Post2</div>
+            </div>
+        </div>
+    </div>
+
+}
+
+
+export default Profile
